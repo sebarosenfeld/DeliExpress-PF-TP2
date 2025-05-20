4acea9e788353e89990353b8f136cda4dce0fb5b
alonsojmateo <alonso.jmateo@gmail.com>
2025-05-20 20:09:29 -0300

    Se agrega modelos: order, user y product

---
+++ src/models/Product.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: true 
+  },
+  description: { 
+    type: String, 
+    required: true 
+  },
+  price: { 
+    type: Number, 
+    required: true 
+  },
+  isAvailable: { 
+    type: Boolean, 
+    required: true 
+  }
+}, { timestamps: true });
+
+export default mongoose.model('Product', productSchema);
